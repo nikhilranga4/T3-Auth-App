0c4a82797511735c02ddd2aab59be1e9165fc2ca
Nikhil_Tony21 <87887727+nikhiltony26@users.noreply.github.com>
2025-02-13 00:08:42 +0530

    Update dependencies and remove deprecated packages

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -12,14 +12,5 @@ export const createQueryClient = () =>
         // above 0 to avoid refetching immediately on the client
         staleTime: 30 * 1000,
       },
-      dehydrate: {
-        serializeData: SuperJSON.serialize,
-        shouldDehydrateQuery: (query) =>
-          defaultShouldDehydrateQuery(query) ||
-          query.state.status === "pending",
-      },
-      hydrate: {
-        deserializeData: SuperJSON.deserialize,
-      },
     },
   });
